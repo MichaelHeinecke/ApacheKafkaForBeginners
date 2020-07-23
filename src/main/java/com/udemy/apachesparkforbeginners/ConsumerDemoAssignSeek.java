4a92f23effd8e84c415b002edda11cc923813792
BoozieBoi <michael.heinecke.1337@gmail.com>
2020-07-23 21:44:05 +0200

    Consumer using assign and seek API to replay data

---
+++ src/main/java/com/udemy/apachesparkforbeginners/ConsumerDemoAssignSeek.java
@@ -0,0 +1,65 @@
+package com.udemy.apachesparkforbeginners;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoAssignSeek {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class);
+
+        String bootstrapServers = "localhost:9092";
+        String topic = "first_topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // assign and seek are mostly used to replay data or fetch specific message
+        // assign
+        TopicPartition partitionToReadFrom = new TopicPartition(topic, 1);
+        long offsetToReadFrom = 15L;
+        consumer.assign(Collections.singleton(partitionToReadFrom));
+
+        // seek
+        consumer.seek(partitionToReadFrom, offsetToReadFrom);
+
+        int numberOfMessagesToRead = 5;
+        int numberOfMessagesRead = 0;
+
+        // poll for new data
+        readLoop: while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                numberOfMessagesRead++;
+
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+
+                if (numberOfMessagesRead >= numberOfMessagesToRead) {
+                    break readLoop;
+                }
+            }
+        }
+
+        logger.info("Exiting application");
+    }
+
+}
